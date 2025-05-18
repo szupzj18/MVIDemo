1651c851cb16f614eae1d10ff340ab72f0f39542
ChrisPan <szupzj18@outlook.com>
2025-05-18 13:40:27 +0800

    define sealed network interface

--- app/src/main/java/com/example/mvidemo/data/Network.kt
+++ app/src/main/java/com/example/mvidemo/data/Network.kt
@@ -1,4 +1,18 @@
 package com.example.mvidemo.data
 
-class Network {
+import android.widget.Toast
+
+sealed interface Result<out T> {
+    data class Success<T>(val data: T)         : Result<T>
+    data class Error(val throwable: Throwable) : Result<Nothing>
+    object Loading                             : Result<Nothing>
+}
+
+class User {
+}
+
+fun handle(result: Result<User>) = when (result) {
+    is Result.Success -> {}
+    is Result.Error -> Toast.makeText(null, result.throwable.message, Toast.LENGTH_SHORT).show()
+    is Result.Loading -> { /** show loading */ }
 }
